b5f07c450f8063f0d03574a4da54351bbb0a610e
Bentein <benteinvathnethomassen@gmail.com>
2018-04-24 17:31:03 +0200

    separated home page into several components

--- lib/pages/MyHomePage.dart
+++ lib/pages/MyHomePage.dart
@@ -33,290 +33,241 @@ class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
 
   Widget body = new WordPage.base(wm.wotd);
   
-  /*new FutureBuilder<List<Word>>(
-    future: wm.wotdFuture,
-    builder: (BuildContext context, AsyncSnapshot<List<Word>> snapshot) {
-      switch (snapshot.connectionState) {
-        case ConnectionState.none: return new Text('Press button to start');
-        case ConnectionState.waiting: return new Center(
-            child: new Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                new CircularProgressIndicator(
-                  strokeWidth: 4.0,
-                ),
-                new Padding(
-                  padding: EdgeInsets.only(bottom: 10.0),
-                ),
-                new Text(
-                  "Henter dagens ord",
-                  style: Theme.of(context).textTheme.display1,
-                )
-              ],
-            ),
-          );
-        
-        default:
-          if (snapshot.hasError) return new Text('Error: ${snapshot.error}');
-          else if (snapshot.data.length == 0) {
-            return new WordPage.base(null);
-          } else if (snapshot.data.length > 0) {
-            return new WordPage.base(snapshot.data[0]);
-          } else {
-            return new WordPage.base(wm.wordList[0]);
-          }
+  Widget getFilterTile(String filter) {
+    return new CheckboxListTile(
+      value: filters.contains(filter),
+      dense: true,
+      title: new Text(filter),
+      onChanged: (bool b) {
+        if (b) { filters.add(filter); }
+        else { filters.remove(filter); }
+        setState(() {
+          body = new SearchPage(results: results, filters: filters);
+        });
       }
-    }
-  ); */
+    );
+  }
 
   @override
   Widget build(BuildContext context) {    
     MaterialColor selectedColor = Colors.deepPurple;
 
-    return new Scaffold(
-      appBar: new AppBar(
-        title: (!search 
-        ? new Text(title)
-        : new TextField(
-          autofocus: true,
-          onSubmitted: (String input) {
-            results = dao.searchWords(input); 
-            setState(() {
-              body = new SearchPage(results: results, filters: filters);
-              search = false;
-            });
-          },
-        )),
-        actions: (body is SearchPage
-          ? <Widget>[
-            new IconButton(
-              icon: new Icon(Icons.search),
-              tooltip: "Søk",
-              onPressed: () {
-                setState(() {
-                  search = true;
-                });
-              },
-            ),
-            new Builder(
-              builder: (BuildContext context) {
-                return new IconButton(
-                  icon: new Icon(Icons.filter_list),
-                  tooltip: "Filter",
-                  onPressed: () {
-                    Scaffold.of(context).openEndDrawer();
-                    search = false;
-                  },
-                );
-              },
-            ),
-            
-          ]
-        : null),
-      ),
-      endDrawer: (body is SearchPage
-        ? new Drawer(
-          child: new ListView(
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              new DrawerHeader(
-                child: new Text("Filtre"),
-                decoration: new BoxDecoration(
-                  color: Theme.of(context).primaryColor
-                ),
-              ),
-              new CheckboxListTile(
-                value: filters.contains("Substantiv"),
-                dense: true,
-                title: new Text("Substantiv"),
-                onChanged: (bool b) {
-                  if (b) { filters.add("Substantiv"); }
-                  else { filters.remove("Substantiv"); }
-                  setState(() {
-                    body = new SearchPage(results: results, filters: filters);             
-                  });
-                },
-              ),
-              new CheckboxListTile(
-                value: filters.contains("Adjektiv"),
-                dense: true,
-                title: new Text("Adjektiv"),
-                onChanged: (bool b) {
-                  if (b) { filters.add("Adjektiv"); }
-                  else { filters.remove("Adjektiv"); }
-                  setState(() {
-                    body = new SearchPage(results: results, filters: filters);
-                  });
-                },
-              ),
-              new CheckboxListTile(
-                value: filters.contains("Verb"),
-                dense: true,
-                title: new Text("Verb"),
-                onChanged: (bool b) {
-                  if (b) { filters.add("Verb"); }
-                  else { filters.remove("Verb"); }
-                  setState(() {
-                    body = new SearchPage(results: results, filters: filters);
-                  });
-                },
-              ),
-              new Divider(),
-            ],
+    Widget appbar = new AppBar(
+      title: (!search 
+      ? new Text(title)
+      : new TextField(
+        autofocus: true,
+        onSubmitted: (String input) {
+          results = dao.searchWords(input); 
+          setState(() {
+            body = new SearchPage(results: results, filters: filters);
+            search = false;
+          });
+        },
+      )),
+      actions: (body is SearchPage
+        ? <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.search),
+            tooltip: "Søk",
+            onPressed: () {
+              setState(() {
+                search = true;
+              });
+            },
           ),
-        )
-        : null),
-      drawer: new Drawer(
-        child: new ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            new DrawerHeader(
-              child: new Text("Naviger"),
-              decoration: new BoxDecoration(
-                color: Theme.of(context).primaryColor
-              ),
-            ),
-            new ListTile(
-              leading: new Icon(
-                Icons.bookmark_border,
-                color: (body is WordPage || body is FutureBuilder<List<Word>> ? selectedColor : Colors.black),
-              ),
-              title: new Text(
-                "Dagens Ord",
-                style: new TextStyle(
-                  color: (body is WordPage || body is FutureBuilder<List<Word>> ? selectedColor : Colors.black)
-                ), 
-              ),
-              onTap: () {
-                setState(() {
-                  title = "Dagens Ord";
+          new Builder(
+            builder: (BuildContext context) {
+              return new IconButton(
+                icon: new Icon(Icons.filter_list),
+                tooltip: "Filter",
+                onPressed: () {
+                  Scaffold.of(context).openEndDrawer();
                   search = false;
-                  body = new WordPage.base(wm.wotd);
-                  Navigator.pop(context);
-                });
-              },
-            ),
-            new ListTile(
-              leading: new Icon(
-                Icons.history,
-                color: (body is HistoryPage ? selectedColor : Colors.black),
-              ),
-              title: new Text(
-                "Historie",
-                style: new TextStyle(
-                  color: (body is HistoryPage ? selectedColor : Colors.black)
-                ), 
-              ),
-              onTap: () {
-                setState(() {
-                  title = "Historie";
-                  search = false;
-                  if (body is! HistoryPage) body = new HistoryPage();
-                  Navigator.pop(context);           
-                });
-              },
-            ),
-            new ListTile(
-              leading: new Icon(
-                Icons.favorite_border,
-                color: (body is FavoritesPage ? selectedColor : Colors.black),
-              ),
-              title: new Text(
-                "Favoritter",
-                style: new TextStyle(
-                  color: (body is FavoritesPage ? selectedColor : Colors.black)
-                ),
-              ),
-              onTap: () {
-                setState(() {
-                  title = "Favoritter";
-                  search = false;
-                  if (body is! FavoritesPage) body = new FavoritesPage();
-                  Navigator.pop(context);                  
-                });
-              },
-            ),
-            new ListTile(
-              leading: new Icon(
-                Icons.search,
-                color: (body is SearchPage ? selectedColor : Colors.black),
-              ),
-              title: new Text(
-                "Søk",
-                style: new TextStyle(
-                  color: (body is SearchPage ? selectedColor : Colors.black)
-                ),
-              ),
-              onTap: () {
-                setState(() {
-                  title = "Søk";
-                  search = false;
-                  results = null;
-                  if (body is! SearchPage) body = new SearchPage();
-                  Navigator.pop(context);
-                });
-              },
-            ),
-            new Divider(),
-            new ListTile(
-              leading: new Icon(
-                Icons.settings,
-                color: (body is Word ? selectedColor : Colors.black),
-              ),
-              title: new Text(
-                "Instillinger",
-                style: new TextStyle(
-                  color: (body is Word ? selectedColor : Colors.black)
-                ),
-              ),
-              onTap: () {
+                },
+              );
+            },
+          ),
+          
+        ]
+      : null),
+    );
 
-              },
+    Widget drawer = new Drawer(
+      child: new ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          new DrawerHeader(
+            child: new Text("Naviger"),
+            decoration: new BoxDecoration(
+              color: Theme.of(context).primaryColor
             ),
-            new ListTile(
-              leading: new Icon(
-                Icons.feedback,
-                color: (body is FeedbackPage ? selectedColor : Colors.black),
-              ),
-              title: new Text(
-                "Send tilbakemelding",
-                style: new TextStyle(
-                  color: (body is FeedbackPage ? selectedColor : Colors.black)
-                ), 
-              ),
-              onTap: () {
-                setState(() {
-                  title = "Tilbakemelding";
-                  search = false;
-                  if (body is! FeedbackPage) body = new FeedbackPage();
-                  Navigator.pop(context);
-                });
-              },
+          ),
+          new ListTile(
+            leading: new Icon(
+              Icons.bookmark_border,
+              color: (body is WordPage || body is FutureBuilder<List<Word>> ? selectedColor : Colors.black),
             ),
-            new ListTile(
-              leading: new Icon(
-                Icons.help,
-                color: (body is HelpPage ? selectedColor : Colors.black),
-              ),
-              title: new Text(
-                "Hjelp",
-                style: new TextStyle(
-                  color: (body is HelpPage ? selectedColor : Colors.black)
-                ),
-              ),
-              onTap: () {
-                setState(() {
-                  title = "Hjelp";
-                  search = false;
-                  if (body is! HelpPage) body = new HelpPage();
-                  Navigator.pop(context);
-                });
-              },
+            title: new Text(
+              "Dagens Ord",
+              style: new TextStyle(
+                color: (body is WordPage || body is FutureBuilder<List<Word>> ? selectedColor : Colors.black)
+              ), 
             ),
-          ],
-        ),
-        
+            onTap: () {
+              setState(() {
+                title = "Dagens Ord";
+                search = false;
+                body = new WordPage.base(wm.wotd);
+                Navigator.pop(context);
+              });
+            },
+          ),
+          new ListTile(
+            leading: new Icon(
+              Icons.history,
+              color: (body is HistoryPage ? selectedColor : Colors.black),
+            ),
+            title: new Text(
+              "Historie",
+              style: new TextStyle(
+                color: (body is HistoryPage ? selectedColor : Colors.black)
+              ), 
+            ),
+            onTap: () {
+              setState(() {
+                title = "Historie";
+                search = false;
+                if (body is! HistoryPage) body = new HistoryPage();
+                Navigator.pop(context);           
+              });
+            },
+          ),
+          new ListTile(
+            leading: new Icon(
+              Icons.favorite_border,
+              color: (body is FavoritesPage ? selectedColor : Colors.black),
+            ),
+            title: new Text(
+              "Favoritter",
+              style: new TextStyle(
+                color: (body is FavoritesPage ? selectedColor : Colors.black)
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                title = "Favoritter";
+                search = false;
+                if (body is! FavoritesPage) body = new FavoritesPage();
+                Navigator.pop(context);                  
+              });
+            },
+          ),
+          new ListTile(
+            leading: new Icon(
+              Icons.search,
+              color: (body is SearchPage ? selectedColor : Colors.black),
+            ),
+            title: new Text(
+              "Søk",
+              style: new TextStyle(
+                color: (body is SearchPage ? selectedColor : Colors.black)
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                title = "Søk";
+                search = false;
+                results = null;
+                if (body is! SearchPage) body = new SearchPage();
+                Navigator.pop(context);
+              });
+            },
+          ),
+          new Divider(),
+          new ListTile(
+            leading: new Icon(
+              Icons.settings,
+              color: (body is Word ? selectedColor : Colors.black),
+            ),
+            title: new Text(
+              "Instillinger",
+              style: new TextStyle(
+                color: (body is Word ? selectedColor : Colors.black)
+              ),
+            ),
+            onTap: () {
+              
+            },
+          ),
+          new ListTile(
+            leading: new Icon(
+              Icons.feedback,
+              color: (body is FeedbackPage ? selectedColor : Colors.black),
+            ),
+            title: new Text(
+              "Send tilbakemelding",
+              style: new TextStyle(
+                color: (body is FeedbackPage ? selectedColor : Colors.black)
+              ), 
+            ),
+            onTap: () {
+              setState(() {
+                title = "Tilbakemelding";
+                search = false;
+                if (body is! FeedbackPage) body = new FeedbackPage();
+                Navigator.pop(context);
+              });
+            },
+          ),
+          new ListTile(
+            leading: new Icon(
+              Icons.help,
+              color: (body is HelpPage ? selectedColor : Colors.black),
+            ),
+            title: new Text(
+              "Hjelp",
+              style: new TextStyle(
+                color: (body is HelpPage ? selectedColor : Colors.black)
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                title = "Hjelp";
+                search = false;
+                if (body is! HelpPage) body = new HelpPage();
+                Navigator.pop(context);
+              });
+            },
+          ),
+        ],
       ),
+    );
+
+    Widget enddrawer = new Drawer(
+      child: new ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          new DrawerHeader(
+            child: new Text("Filtre"),
+            decoration: new BoxDecoration(
+              color: Theme.of(context).primaryColor
+            ),
+          ),
+          getFilterTile("Substantiv"),
+          getFilterTile("Adjektiv"),
+          getFilterTile("Verb"),
+          new Divider(),
+        ],
+      ),
+    );
+
+    return new Scaffold(
+      appBar: appbar,
+      endDrawer: (body is SearchPage
+        ? enddrawer
+        : null),
+      drawer: drawer,
       body: body,
     );
   }
